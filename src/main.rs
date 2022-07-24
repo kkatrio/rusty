ad84950c629dc98d710accdfdb500bc0cb257915
konstantinos katrioplas <kokatrio@gmail.com>
2022-07-24 17:01:48 +0300

    store the buffer in the heap

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ use std::collections::HashMap;
 #[derive(Debug)]
 enum EventType {
     Accept,
-    Read,
+    Read {
+        buf: Box<[u8]>,
+    },
     Write,
     Poll,
 }
@@ -19,25 +21,25 @@ fn push_poll_entry(sq: &mut SubmissionQueue, fd: i32) -> std::io::Result<()> {
     unsafe {
         sq.push(&poll_e).expect("submission queue is full");
     }
-    println!("PUSHED poll");
+    //println!("PUSHED poll");
     sq.sync();
     Ok(())
 }
 
-fn push_write_entry(sq: &mut SubmissionQueue, fd: i32, buf: &mut [u8; 2048]) -> std::io::Result<()> {
-    let write_e = opcode::Write::new(types::Fd(fd), buf.as_mut_ptr(), buf.len() as _)
+fn push_write_entry(sq: &mut SubmissionQueue, fd: i32, buf: &Box<[u8]>) -> std::io::Result<()> {
+    let write_e = opcode::Write::new(types::Fd(fd), buf.as_ptr(), buf.len() as _)
         .build()
         .user_data(fd.try_into().unwrap());
 
     unsafe {
         sq.push(&write_e).expect("submission queue is full");
     }
-    println!("PUSHED write");
+    //println!("PUSHED write");
     sq.sync();
     Ok(())
 }
 
-fn push_read_entry(sq: &mut SubmissionQueue, fd: i32, buf: &mut [u8; 2048]) -> std::io::Result<()> {
+fn push_read_entry(sq: &mut SubmissionQueue, fd: i32, buf: &mut Box<[u8]>) -> std::io::Result<()> {
     let read_e = opcode::Read::new(types::Fd(fd), buf.as_mut_ptr(), buf.len() as _)
         .build()
         .user_data(fd.try_into().unwrap());
@@ -45,7 +47,7 @@ fn push_read_entry(sq: &mut SubmissionQueue, fd: i32, buf: &mut [u8; 2048]) -> s
     unsafe {
         sq.push(&read_e).expect("submission queue is full");
     }
-    println!("PUSHED read");
+    //println!("PUSHED read");
     sq.sync();
     Ok(())
 }
@@ -58,14 +60,12 @@ fn push_accept_entry(sq: &mut SubmissionQueue, fd: i32,  sa: &mut libc::sockaddr
     unsafe {
         sq.push(&accept_e).expect("submission queue is full");
     }
-    println!("PUSHED accept");
+    //println!("PUSHED accept");
     sq.sync();
     Ok(())
 }
 
 fn main() -> std::io::Result<()> {
-
-    let mut buf = [0u8; 2048];
 
     let listener = TcpListener::bind("0.0.0.0:8090").unwrap();
     println!("listening on {}", listener.local_addr()?);
@@ -80,7 +80,7 @@ fn main() -> std::io::Result<()> {
         sa_data: [0; 14] };
     let mut sockaddrlen = size_of::<libc::sockaddr_in>() as libc::socklen_t;
 
-    // keep track of event per connection
+    // keep track of event per connection <fd, EventType>
     let mut fd_event = HashMap::new();
 
     push_accept_entry(&mut sq, fd, &mut sockaddr, &mut sockaddrlen)?;
@@ -96,7 +96,7 @@ fn main() -> std::io::Result<()> {
             Err(err) => return Err(err.into()), }
         cq.sync();
         sq.sync();
-        println!("len after submit: sq={} cq= {}", sq.len(), cq.len());
+        println!("len after submit : sq={} cq= {}", sq.len(), cq.len());
 
         if accept_on {
             push_accept_entry(&mut sq, fd, &mut sockaddr, &mut sockaddrlen)?;
@@ -111,12 +111,11 @@ fn main() -> std::io::Result<()> {
             // contains the fd that was pushed as user data
             let conn_fd = cqe.user_data() as i32;
 
-            println!("retval: {}", retval);
-            println!("conn_fd: {}", conn_fd);
+            //println!("retval: {}", retval);
+            //println!("conn_fd: {}", conn_fd);
 
             // what event completed on this socket
             let event_type = &fd_event[&conn_fd]; // i32
-            println!("event_type: {:?}", event_type);
 
             match event_type {
                 EventType::Accept => {
@@ -127,18 +126,18 @@ fn main() -> std::io::Result<()> {
                     push_poll_entry(&mut sq, retval)?;
                 }
                 EventType::Poll => {
+                    let mut buf = vec![0u8; 2048].into_boxed_slice();
                     push_read_entry(&mut sq, conn_fd, &mut buf)?;
-                    fd_event.insert(conn_fd, EventType::Read);
+                    fd_event.insert(conn_fd, EventType::Read {buf});
 
                 }
-                EventType::Read => {
-                    println!("conn_fd in Read: {}", conn_fd);
-                    push_write_entry(&mut sq, conn_fd, &mut buf)?;
+                EventType::Read {buf} => {
+                    //println!("conn_fd in Read: {}", conn_fd);
+                    push_write_entry(&mut sq, conn_fd, &buf)?;
                     fd_event.insert(conn_fd, EventType::Write);
 
                 }
                 EventType::Write => {
-                    println!("buffer:  {}", String::from_utf8_lossy(&buf));
                     // check if there is another message to read
                     push_poll_entry(&mut sq, conn_fd)?;
                     fd_event.insert(conn_fd, EventType::Poll);

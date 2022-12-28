ff0625db6cc520cc72b9bb4eb4cf0f26c8adc54e
konstantinos katrioplas <kokatrio@gmail.com>
2022-12-28 20:20:28 +0200

    clean

--- src/server.rs
+++ src/server.rs
@@ -69,9 +69,9 @@ impl Server {
         })
     }
 
+    // fd is the connected socket, buf is the received message
     fn handle_request(fd: i32, buf: &Box<[u8]>, fd_resp_map: &mut HashMap<i32, String>) {
         let get = b"GET / HTTP/1.1\r\n";
-        log::info!("SELF function used");
         let (status_line, filename) = if buf.starts_with(get) {
             ("HTTP/1.1 200 OK", "resources/hello.html")
         } else {
